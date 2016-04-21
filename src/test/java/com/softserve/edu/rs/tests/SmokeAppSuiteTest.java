6cb33927c1fed9eb8f51c1fec9b19e18323ee368
softservedata <wapgprs@list.ru>
2016-04-21 13:22:57 +0300

    Update SmokeTest

--- src/test/java/com/softserve/edu/rs/tests/SmokeAppSuiteTest.java
+++ src/test/java/com/softserve/edu/rs/tests/SmokeAppSuiteTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.softserve.edu.rs.tests;
 
 import java.util.HashMap;
@@ -58,7 +57,7 @@ public class SmokeAppSuiteTest {
         if (language == ChangeLanguageFields.UKRAINIAN) {
         	System.out.println("UKRAINIAN found");
         }
-        for (ChangeLanguageFields all : language.values()) {
+        for (ChangeLanguageFields all : ChangeLanguageFields.values()) {
         	System.out.println("all = "+all);
         }
 	}
@@ -85,74 +84,3 @@ public class SmokeAppSuiteTest {
 	}
 
 }
-=======
-package com.softserve.edu.rs.tests;
-
-import java.util.HashMap;
-
-import org.testng.Assert;
-import org.testng.ITestContext;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
-import com.softserve.edu.rs.data.apps.ApplicationSources;
-import com.softserve.edu.rs.data.apps.ApplicationSourcesRepository;
-import com.softserve.edu.rs.data.users.IUser;
-import com.softserve.edu.rs.data.users.UserRepository;
-import com.softserve.edu.rs.pages.AdminHomePage;
-import com.softserve.edu.rs.pages.Application;
-
-public class SmokeAppSuiteTest {
-
-    @DataProvider//(parallel = true)
-    public Object[][] adminUsers(ITestContext context) {
-        System.out.println("@DataProvider adminUsers, Thread Id = "
-                + Thread.currentThread().getId());
-        ApplicationSources applicationSources = ApplicationSourcesRepository.get().getLocalHostByFirefox();
-        //
-        // Print all parameters
-        HashMap<String, String> hashMap = new HashMap<String, String>(context.getCurrentXmlTest().getSuite().getParameters()); 
-        for (String key : hashMap.keySet()) {
-        	System.out.println("Suite parameter: key=" + key + " value=" + hashMap.get(key));
-        }
-        //
-        if (context.getCurrentXmlTest().getSuite().getParameter("loginurl") != null) {
-            System.out.println("New loginurl: "
-                    + context.getCurrentXmlTest().getSuite().getParameter("loginurl"));
-            applicationSources.setLoginUrl(context.getCurrentXmlTest().getSuite().getParameter("loginurl"));
-        }
-        if (context.getCurrentXmlTest().getSuite().getParameter("logouturl") != null) {
-            System.out.println("New loginurl: "
-                    + context.getCurrentXmlTest().getSuite().getParameter("logouturl"));
-            applicationSources.setLogoutUrl(context.getCurrentXmlTest().getSuite().getParameter("logouturl"));
-        }
-        //
-        Application application = new Application(applicationSources);
-        return new Object[][] {
-                { application, UserRepository.get().getAdmin() },
-                //{ application, UserRepository.get().getCommissioner() },
-                //{ application, UserRepository.get().getAdmin() },
-                };
-    }
- 
-	@Test(dataProvider = "adminUsers")
-	public void adminLogin1(Application application, IUser admin) throws Exception {
-        System.out.println("@Test adminLogin1, Thread Id = "
-                + Thread.currentThread().getId());
-		// Preconditions.
-		// Test Steps.
-		AdminHomePage adminHomePage = application.load()
-				.successAdminLogin(admin); 
-		// Checking.
-		Thread.sleep(2000);
-		Assert.assertEquals(admin.getAccount().getLogin(),
-				adminHomePage.getLoginAccountText());
-		// Return to previous state.
-		adminHomePage.logout();
-		//
-		Thread.sleep(2000);
-		application.quit();
-	}
-
-}
->>>>>>> 88e6fe380f7d78b9b97c719ffba189e7ed5f26ea
