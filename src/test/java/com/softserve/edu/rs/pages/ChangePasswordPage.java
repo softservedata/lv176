2c94c6724d6fee6995a3276f60f00f902029bf1c
Stas Lomakov <stas.lomakov@gmail.com>
2016-05-13 14:30:18 +0300

    demo3

--- src/test/java/com/softserve/edu/rs/pages/ChangePasswordPage.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.edu.rs.pages;
-
-import org.openqa.selenium.WebDriver;
-
-public class ChangePasswordPage extends TopPage {
-	public ChangePasswordPage(WebDriver driver) {
-		super(driver);
-	}
-}
