ed16f539d4023f6e917f1be301f66b147d87d447
softservedata <wapgprs@list.ru>
2016-05-16 19:00:40 +0300

    Add Criteria classes

---
+++ src/main/java/com/softserve/edu/atqc/specs/LinkCriteria.java
@@ -0,0 +1,47 @@
+package com.softserve.edu.atqc.specs;
+
+import com.softserve.edu.atqc.controls.ILink;
+
+public final class LinkCriteria extends ABaseCriteria implements ILinkCriteriaNext {
+	private final String DOES_NOT_MATCH = "Values doesn't match. Expected Result %s ActualResult %s";
+	private final String IT_IS_NOT_VISIBLE = "Link it's not visible";
+	private ILink link;
+
+	private LinkCriteria(ILink link) {
+		this.link = link;
+	}
+
+	public static ILinkCriteria get(ILink link) {
+		return new LinkCriteria(link);
+	}
+
+	public ILinkCriteriaNext urlMatch(String expectedResult) {
+	    FlexAssert.get().verify(link.getUrl().equals(expectedResult),
+				String.format(DOES_NOT_MATCH, expectedResult, link.getUrl()));
+		return this;
+	}
+
+	public ILinkCriteriaNext valueMatch(String expectedResult) {
+	    FlexAssert.get().verify(link.getText().equals(expectedResult),
+				String.format(DOES_NOT_MATCH, expectedResult, link.getText()));
+		return this;
+	}
+
+	public ILinkCriteriaNext valueStartsWith(String expectedResult) {
+	    FlexAssert.get().verify(link.getText().startsWith(expectedResult),
+				String.format(DOES_NOT_MATCH, expectedResult, link.getText()));
+		return this;
+	}
+
+	public ILinkCriteriaNext valueByPartialText(String expectedResult) {
+	    FlexAssert.get().verify(link.getText().contains(expectedResult),
+				String.format(DOES_NOT_MATCH, expectedResult, link.getText()));
+		return this;
+	}
+
+	public ILinkCriteriaNext  isVisible() {
+	    FlexAssert.get().verify(link.isDisplayed(), IT_IS_NOT_VISIBLE);
+		return this;
+	}
+
+}
